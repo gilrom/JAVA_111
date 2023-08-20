aaa2658209180ca35bb2e20f42860f240b211836
roisasson2 <sasson@campus.technion.ac.il>
2023-08-20 13:57:25 +0300

    Q2_files

---
+++ Q2/src/Vehicle.java
@@ -0,0 +1,31 @@
+public abstract class Vehicle {
+    private int GPK;
+    private String manufacturer;
+    private String color;
+    private int volume;
+    private int gas;
+    public Vehicle (int gpk, String manufacturer, String color, int volume){
+        this.GPK = gpk;
+        this.manufacturer = manufacturer;
+        this. color = color;
+        this.volume = volume;
+        this.gas = volume;
+    }
+    @Override
+    public String toString() {
+        return this.color + " " + this.manufacturer + " " + this.gas + "/" + this.volume;
+    }
+    public boolean drive(float dist){
+        if((dist * this.GPK) <= this.gas){
+            this.gas -= dist*this.GPK;
+            return true;
+        }
+        return  false;
+    }
+    public float fillGas(float price_per_liter){
+        float total_price = (this.volume - this.gas) * price_per_liter;
+        this.gas = this.volume;
+        return total_price;
+    }
+    public abstract  void changeTires();
+}
