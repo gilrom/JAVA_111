81f9119960ea6d8255e54b26f4e985363ad15920
javaexp <gilrom@gmail.com>
2023-08-20 13:49:27 +0300

    Q2

--- src/Motorcycle.java
+++
@@ -1,9 +0,0 @@
-public class Motorcycle extends Vehicle {
-    public Motorcycle (int gpk, String manufacturer, String color, int volume){
-        super(gpk, manufacturer, color, volume);
-    }
-    @Override
-    public void changeTires(){
-        System.out.println("2");
-    }
-}
